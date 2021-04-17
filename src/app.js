b10c95efc1ada8fa6bb7a168b4bd71ebc8c13f21
Benoit Welsch <bwelsch@helb-prigogine.be>
2021-04-17 14:54:28 +0200

    Refactor :  Dead code removed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-// const ddl = require('./ddl');
-
-// for (let i = 0; i < 10; i++) {
-//   let url = "https://www.stream-vf.co/anime/one-piece-vostfr/episode-" + (390 + i)
-//   ddl(url)
-// }
-
 const Episode = require('./episode');
 const Manager = require('./manager');
 
