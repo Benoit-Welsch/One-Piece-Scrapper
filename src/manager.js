1aa489b7c7f321f2d200aface9eccf4eca45f6db
Benoit Welsch <bwelsch@helb-prigogine.be>
2021-04-21 10:57:48 +0200

    Fix : Adapt simultaneous ddl if bigger than episode

--- src/manager.js
+++ src/manager.js
@@ -45,6 +45,7 @@ class Manager {
 
   download() {
     if (this.episodes.length == 0) return
+    this.simultaneousDl = this.episodes.length < this.simultaneousDl ? this.episodes.length : this.simultaneousDl;
 
     let
       start = 0,
@@ -86,6 +87,57 @@ class Manager {
           console.log(err.message)
         })
       promise.push(ddl)
+
+    }
+  }
+
+  simulate() {
+    if (this.episodes.length == 0) return
+    this.simultaneousDl = this.episodes.length < this.simultaneousDl ? this.episodes.length : this.simultaneousDl;
+
+    console.log('test')
+
+    let
+      start = 0,
+      stop = this.simultaneousDl,
+      promise = [];
+
+    console.log('Total episode          :  ' + this.episodes.length);
+    console.log('Simultaneous download  :  ' + this.simultaneousDl);
+    console.log();
+    console.log('Download in progress ...');
+
+    const ddlNextEpisode = () => {
+      // Find the next episode to download 
+      let episode = this.episodes.find(episode => episode.isReady());
+      if (episode) {
+
+        let ddl = episode.simulate(this.path, this.multibar)
+          .then(() => {
+            ddlNextEpisode();
+          })
+          .catch(err => {
+            console.log(err.message)
+          })
+
+        promise.push(ddl)
+      }
+    }
+
+    // Start download of a range of episode 
+    for (let index = start; index < stop; index++) {
+      //this.promise.push(this.episodes[index].start);
+
+      let ddl = this.episodes[index]
+        .simulate(this.path, this.multibar)
+        .then(() => {
+          ddlNextEpisode()
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+      promise.push(ddl)
+
 
     }
   }
